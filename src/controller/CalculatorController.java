bfd78762ecb9540a2d5ce056ea1f3e9eb6e36372
Jurca Alexandru - Cristian <jurcaalex42@gmail.com>
2022-06-19 12:22:05 +0200

    v. 0.2

--- src/controller/CalculatorController.java
+++ src/controller/CalculatorController.java
@@ -2,137 +2,139 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class CalculatorController implements Initializable {
     Double data = 0.0;
     int operation = -1;
 
-    @FXML
-    private Button minus;
+    @FXML private Button keyMinus;
+    @FXML private Button keyNine;
+    @FXML private Button keySix;
+    @FXML private Button keyMult;
+    @FXML private Button keyOne;
+    @FXML private Button clear;
+    @FXML private Button keySeven;
+    @FXML private Button keyTwo;
+    @FXML private Button keyThree;
+    @FXML private Button keyPlus;
+    @FXML private Button keyEight;
+    @FXML private Button keyZero;
+    @FXML private Button keyDiv;
+    @FXML private Button keyFour;
+    @FXML private Button keyDot;
+    @FXML private Button keyEquals;
+    @FXML private Button keyFive;
+
+    @FXML private TextField result;
+
+    @FXML 
+    void entered(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        button.setCursor(Cursor.HAND);
+    }
 
     @FXML
-    private Button nine;
+    void exited(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        button.setCursor(Cursor.DEFAULT);
+    }
 
     @FXML
-    private Button six;
+    void utilEntered(MouseEvent event) {
+        ImageView img = (ImageView) event.getSource();
+        img.setCursor(Cursor.HAND);
+    }
 
     @FXML
-    private Button mult;
+    void utilExited(MouseEvent event) {
+        ImageView img = (ImageView) event.getSource();
+        img.setCursor(Cursor.DEFAULT);
+    }
+    
+    @FXML
+    void close(MouseEvent event) {
+        Platform.exit();
+    }
 
     @FXML
-    private Button one;
-
-    @FXML
-    private TextField result;
-
-    @FXML
-    private Button clear;
-
-    @FXML
-    private Button seven;
-
-    @FXML
-    private Label label;
-
-    @FXML
-    private Button two;
-
-    @FXML
-    private Button three;
-
-    @FXML
-    private Button plus;
-
-    @FXML
-    private Button eight;
-
-    @FXML
-    private Button zero;
-
-    @FXML
-    private Button div;
-
-    @FXML
-    private Button four;
-
-    @FXML
-    private Button dot;
-
-    @FXML
-    private Button equals;
-
-    @FXML
-    private Button five;
+    void minimize(MouseEvent event) {
+        Stage stage = (Stage) ((ImageView) event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
 
     @FXML
     void handle(ActionEvent event) {
-        if (event.getSource() == one) {
+        if (event.getSource() == keyOne) {
             result.setText(result.getText() + "1");
-        } else if (event.getSource() == two) {
+        } else if (event.getSource() == keyTwo) {
             result.setText(result.getText() + "2");
-        } else if (event.getSource() == three) {
+        } else if (event.getSource() == keyThree) {
             result.setText(result.getText() + "3");
-        } else if (event.getSource() == four) {
+        } else if (event.getSource() == keyFour) {
             result.setText(result.getText() + "4");
-        } else if (event.getSource() == five) {
+        } else if (event.getSource() == keyFive) {
             result.setText(result.getText() + "5");
-        } else if (event.getSource() == six) {
+        } else if (event.getSource() == keySix) {
             result.setText(result.getText() + "6");
-        } else if (event.getSource() == seven) {
+        } else if (event.getSource() == keySeven) {
             result.setText(result.getText() + "7");
-        } else if (event.getSource() == eight) {
+        } else if (event.getSource() == keyEight) {
             result.setText(result.getText() + "8");
-        } else if (event.getSource() == nine) {
+        } else if (event.getSource() == keyNine) {
             result.setText(result.getText() + "9");
-        } else if (event.getSource() == zero) {
+        } else if (event.getSource() == keyZero) {
             result.setText(result.getText() + "0");
-        } else if (event.getSource() == dot) {
+        } else if (event.getSource() == keyDot) {
             result.setText(result.getText() + ".");
         } else if (event.getSource() == clear) {
             result.setText("");
-        } else if (event.getSource() == plus) {
+        } else if (event.getSource() == keyPlus) {
             data = Double.parseDouble(result.getText());
             operation = 1;
             result.setText("");
-        } else if (event.getSource() == minus) {
+        } else if (event.getSource() == keyMinus) {
             data = Double.parseDouble(result.getText());
             operation = 2;
             result.setText("");
-        } else if (event.getSource() == mult) {
+        } else if (event.getSource() == keyMult) {
             data = Double.parseDouble(result.getText());
             operation = 3;
             result.setText("");
-        } else if (event.getSource() == div) {
+        } else if (event.getSource() == keyDiv) {
             data = Double.parseDouble(result.getText());
             operation = 4;
             result.setText("");
-        } else if (event.getSource() == equals) {
+        } else if (event.getSource() == keyEquals) {
             Double secondOperand = Double.parseDouble(result.getText());
             switch (operation) {
                 case 1:
-                    Double ans = data + secondOperand;
-                    result.setText(String.valueOf(ans));
+                    Double answer = data + secondOperand;
+                    result.setText(String.valueOf(answer));
                     break;
                 case 2:
-                    ans = data - secondOperand;
-                    result.setText(String.valueOf(ans));
+                    answer = data - secondOperand;
+                    result.setText(String.valueOf(answer));
                     break;
                 case 3:
-                    ans = data * secondOperand;
-                    result.setText(String.valueOf(ans));
+                    answer = data * secondOperand;
+                    result.setText(String.valueOf(answer));
                     break;
                 case 4:
-                    ans = 0.0;
+                    answer = 0.0;
                     try {
-                        ans = data / secondOperand;
+                        answer = data / secondOperand;
                     } catch(Exception e) { result.setText("Error"); }
-                    result.setText(String.valueOf(ans));
+                    result.setText(String.valueOf(answer));
                     break;
             }
         }
